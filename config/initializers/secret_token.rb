ec4425d9a58b55cae36215adeee5ebb3dae05bea
Dan Berke <danb@mobilibuy.com>
2014-04-29 16:42:15 +0300

    Git initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e390a6c3bf2cebe739449ac26596c92675137b37dfacba6c4717a054c614fb4281d975134a630276c5033ab37082a4068a88eaec588d2dd396cccbbfb58dec22'
